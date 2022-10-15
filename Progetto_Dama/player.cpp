9b7e669a12e3c1c512b5d238b7ddd33eeac48a40
FilippoAlpago <891279@stud.unive.it>
2022-10-15 09:42:07 +0200

    15/10/22

--- Progetto_Dama/player.cpp
+++ Progetto_Dama/player.cpp
@@ -5,21 +5,20 @@
 #include <stdlib.h>
 struct mossa
 {
-    int Num_mossa;//numero mossa
+    
     int Campo_gioco [8][8];//campo da gioco di dama
     mossa* next;//mossa succesiva
 };
 struct Player::Impl
 {
-    mossa* history;//history tiene traccia di tutte le mosse
+    mossa* history=nullptr;//history tiene traccia di tutte le mosse
     
 };
 
 Player::Player(int player_nr = 1)
 {
     //prima creo una nuova mossa, poi la attaco alla history
-    
-    int a;
+    mossa* app=new mossa;
 
     
 }
